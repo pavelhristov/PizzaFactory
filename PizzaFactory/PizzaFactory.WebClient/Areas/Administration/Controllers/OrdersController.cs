33698cdcfc5f591bcf0dc88bab7ffdf19b73f12c
pavelhristov <pavel.hristov_@abv.bg>
2017-03-25 15:56:19 +0200

    administration

--- PizzaFactory/PizzaFactory.WebClient/Areas/Administration/Controllers/OrdersController.cs
+++ PizzaFactory/PizzaFactory.WebClient/Areas/Administration/Controllers/OrdersController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PizzaFactory.WebClient.Areas.Administration.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class OrdersController : Controller
     {
         // GET: Administration/Orders
